37a4e214a8b8ed00af87de202110856925987c73
yuyu <swharriet2012@gmail.com>
2023-11-16 23:06:15 +1100

    add play again

--- guessing-game.py
+++ guessing-game.py
@@ -5,10 +5,22 @@
 import random
 
 random_number = random.randint(1,11)
-guess = None
+# guess = None
 
 
-while guess != random_number:
+# while guess != random_number:
+#     guess = input('Pick a number from 1 to 10: ')
+#     guess = int(guess)
+#     if guess < random_number:
+#         print('Too low!')
+#     elif guess > random_number:
+#         print('Too high!')
+#     else: 
+#         print('You got it!')
+    
+# print(random_number)
+
+while True:
     guess = input('Pick a number from 1 to 10: ')
     guess = int(guess)
     if guess < random_number:
@@ -17,6 +29,10 @@ while guess != random_number:
         print('Too high!')
     else: 
         print('You got it!')
-    
-
-print(random_number)
+        play_again = input('Do you want to play again? [y/n] ')
+        if play_again == 'y':
+            random_number = random.randint(1,11)
+            guess = None
+        else:
+            print('Thank you for playing!')
+            break
